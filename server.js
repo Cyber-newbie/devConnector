1dc714fc9c14291b106f42fbc60d4705bdfa5353
Cyber-newbie <hasanhaider071@gmail.com>
2023-10-17 13:40:21 +0500

    deploy to vercel

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport')
 const env = require('dotenv')
+const path = require('path')
 const connectDB = require('./config/db')
 const router = require('./routes/api/users')
 const profileRouter = require('./routes/api/profile')
@@ -32,6 +33,13 @@ app.use('/api/users', router)
 app.use('/api/profile', profileRouter)
 app.use('/api/posts', postRouter)
 app.use(route)
+if (process.env.NODE_ENV === 'production') {
+    //set static file
+    app.use(express.static('client/build'))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 
